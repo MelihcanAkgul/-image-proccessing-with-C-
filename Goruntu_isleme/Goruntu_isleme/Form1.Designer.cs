6467b76aab442a6872ea1a974b6403c61c64a5ea
MelihcanAkgul <140709855+MelihcanAkgul@users.noreply.github.com>
2023-12-21 13:37:48 +0300

    image proccessing
    Finding fruit with image processing

---
+++ Goruntu_isleme/Goruntu_isleme/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Goruntu_isleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Location = new System.Drawing.Point(46, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "İşlenecek Görüntüleri Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button2.Location = new System.Drawing.Point(71, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Meyveyi Seç";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button3.Location = new System.Drawing.Point(71, 276);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 35);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Meyveyi Bul";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(186, 341);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(68, 341);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Çıkan Sonuç:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(101, 163);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(119, 87);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(22, 129);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(36, 30);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.Visible = false;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 396);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Goruntu_isleme/Goruntu_isleme/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Goruntu_isleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] CounterArray = new int[180];
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+
+                listBox1.Items.Clear();
+                string[] dosyalar = Directory.GetFiles(fbd.SelectedPath);
+                foreach (string dosya in dosyalar)
+                {
+                    if (dosya.EndsWith(".jpg") || dosya.EndsWith(".jpeg"))
+                        listBox1.Items.Add(dosya);
+                }
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string resimYolu = listBox1.SelectedItem.ToString();
+            pictureBox1.ImageLocation = resimYolu;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(open.FileName);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap targetpic = new Bitmap(pictureBox1.Image);
+
+            for (int x = 0; x <= 179; x++)
+            {
+
+                Bitmap templatepic = new Bitmap(listBox1.Items[x].ToString());
+
+                int sayac = 0;
+
+                for (int i = 0; i < pictureBox1.Image.Width; i++)
+                {
+                    for (int j = 0; j < pictureBox1.Image.Height; j++)
+                    {
+
+                        Color targetrgb = targetpic.GetPixel(i, j);
+                        Color templatergb = templatepic.GetPixel(i, j);
+
+                        if (targetrgb == templatergb)
+                        {
+                            sayac++;
+                        }
+
+                    }
+                }
+
+                CounterArray[x] = sayac;
+            }
+
+            label1.Visible = true;
+
+            if (Array.IndexOf(CounterArray, 10000) >= 0 && Array.IndexOf(CounterArray, 10000) <= 4)
+            {
+                label1.Text = "Armut";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 5 && Array.IndexOf(CounterArray, 10000) <= 9)
+            {
+                label1.Text = "Avokado";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 10 && Array.IndexOf(CounterArray, 10000) <= 14)
+            {
+                label1.Text = "Ayva";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 15 && Array.IndexOf(CounterArray, 10000) <= 19)
+            {
+                label1.Text = "Kırmızı Biber";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 20 && Array.IndexOf(CounterArray, 10000) <= 24)
+            {
+                label1.Text = "Blueberry";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 25 && Array.IndexOf(CounterArray, 10000) <= 29)
+            {
+                label1.Text = "Ceviz";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 30 && Array.IndexOf(CounterArray, 10000) <= 34)
+            {
+                label1.Text = "Çilek";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 35 && Array.IndexOf(CounterArray, 10000) <= 39)
+            {
+                label1.Text = "Domates";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 40 && Array.IndexOf(CounterArray, 10000) <= 44)
+            {
+                label1.Text = "Ejder Meyvesi";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 45 && Array.IndexOf(CounterArray, 10000) <= 49)
+            {
+                label1.Text = "Bordo Elma";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 50 && Array.IndexOf(CounterArray, 10000) <= 54)
+            {
+                label1.Text = "Kırmızı Elma";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 55 && Array.IndexOf(CounterArray, 10000) <= 59)
+            {
+                label1.Text = "Sarı Elma";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 60 && Array.IndexOf(CounterArray, 10000) <= 64)
+            {
+                label1.Text = "Yeşil Elma";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 65 && Array.IndexOf(CounterArray, 10000) <= 69)
+            {
+                label1.Text = "Fıstık";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 70 && Array.IndexOf(CounterArray, 10000) <= 74)
+            {
+                label1.Text = "Frambuaz";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 75 && Array.IndexOf(CounterArray, 10000) <= 79)
+            {
+                label1.Text = "Hindistan Cevizi";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 80 && Array.IndexOf(CounterArray, 10000) <= 84)
+            {
+                label1.Text = "İncir";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 85 && Array.IndexOf(CounterArray, 10000) <= 89)
+            {
+                label1.Text = "Kaktüs";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 90 && Array.IndexOf(CounterArray, 10000) <= 94)
+            {
+                label1.Text = "Karadut";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 95 && Array.IndexOf(CounterArray, 10000) <= 99)
+            {
+                label1.Text = "Karnıbahar";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 100 && Array.IndexOf(CounterArray, 10000) <= 104)
+            {
+                label1.Text = "Karpuz";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 105 && Array.IndexOf(CounterArray, 10000) <= 109)
+            {
+                label1.Text = "Kavun";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 110 && Array.IndexOf(CounterArray, 10000) <= 114)
+            {
+                label1.Text = "Kiraz";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 115 && Array.IndexOf(CounterArray, 10000) <= 119)
+            {
+                label1.Text = "Limon";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 120 && Array.IndexOf(CounterArray, 10000) <= 124)
+            {
+                label1.Text = "Mandalina";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 125 && Array.IndexOf(CounterArray, 10000) <= 129)
+            {
+                label1.Text = "Mısır";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 130 && Array.IndexOf(CounterArray, 10000) <= 134)
+            {
+                label1.Text = "Muz";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 135 && Array.IndexOf(CounterArray, 10000) <= 139)
+            {
+                label1.Text = "Patlıcan";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 140 && Array.IndexOf(CounterArray, 10000) <= 144)
+            {
+                label1.Text = "Pembeli";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 145 && Array.IndexOf(CounterArray, 10000) <= 149)
+            {
+                label1.Text = "Pepino";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 150 && Array.IndexOf(CounterArray, 10000) <= 154)
+            {
+                label1.Text = "Portakal";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 155 && Array.IndexOf(CounterArray, 10000) <= 159)
+            {
+                label1.Text = "Soğan";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 160 && Array.IndexOf(CounterArray, 10000) <= 164)
+            {
+                label1.Text = "Üzüm";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 165 && Array.IndexOf(CounterArray, 10000) <= 169)
+            {
+                label1.Text = "Yaban Mersini";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 170 && Array.IndexOf(CounterArray, 10000) <= 174)
+            {
+                label1.Text = "Yıldız";
+            }
+            else if (Array.IndexOf(CounterArray, 10000) >= 175 && Array.IndexOf(CounterArray, 10000) <= 179)
+            {
+                label1.Text = "Zencefil";
+            }
+        }
+    }
+}
